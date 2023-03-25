6313b5ef43f0cbb94cdc88ea0ed89a84b421bacf
Rob Schilder <35261620+RobSchilderr@users.noreply.github.com>
2023-03-25 16:23:51 +0100

    fix: remove 'party' vibe type, make no sense

--- packages/ui/components/DropDown.tsx
+++ packages/ui/components/DropDown.tsx
@@ -10,14 +10,14 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
 }
 
-export type VibeType = 'Casual' | 'Professional' | 'Funny' | 'Party'
+export type VibeType = 'Casual' | 'Professional' | 'Funny'
 
 interface DropDownProps {
   vibe: VibeType
   setVibe: (vibe: VibeType) => void
 }
 
-let vibes: VibeType[] = ['Casual', 'Professional', 'Funny', 'Party']
+let vibes: VibeType[] = ['Casual', 'Professional', 'Funny']
 
 export default function DropDown({ vibe, setVibe }: DropDownProps) {
   return (
